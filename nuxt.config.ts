1cf03c2c94baaf35a54ee7c45a10d47297ce5315
Shemmy <n3shemmy3@gmail.com>
2023-10-17 20:12:48 +0200

    added template files

---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  baseURL: "/Notes/",
+  app: {
+    buildAssetsDir: "/app/",
+    baseURL: "/Notes/",
+    head: {
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "https://api.iconify.design/ph:terminal-window.svg?color=%23888888",
+        },
+      ],
+    },
+  },
+
+  plugins: [
+    "@/plugins/vRipple.js"
+  ],
+  css: [
+    "@/assets/css/tailwind.css",
+    "@/assets/fonts/Montserrat.css",
+    "@/assets/fonts/MajorMonoDisplay.css"
+  ],
+})
